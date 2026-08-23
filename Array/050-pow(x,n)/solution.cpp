f84f1d29e09af0250f03bcd9af70d30917adba0b
Vivek Shinde <vivek4141shinde@gmail.com>
2026-08-23 17:11:48 +0530

    Power of element

---
+++ Array/050-pow(x,n)/solution.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            power /= 2;
+        }
+
+        return result;
+    }
+};
