68f6b6903cd7f3b72f8a96638bbeee8192612203
Vivek Shinde <vivek4141shinde@gmail.com>
2026-08-23 16:55:36 +0530

    Finding next permutation of give array

---
+++ Array/031-next-permutation/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Find the first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Find the next greater element
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums[i], nums[j]);
+        }
+
+        // Reverse the remaining part
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
